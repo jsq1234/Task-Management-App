31611b8f08ee5c2d224fb984d39a092fe3864909
jsq1234 <mananprakash3078@gmail.com>
2023-11-27 02:27:12 +0530

    experimental changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
-require('dotenv').config();
+
 const app = express();
+
+require('dotenv').config();
+
 app.use(express.json());
 
+app.use((req, res, next) => {
+    const currentDate = new Date();
+    const options = {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    }
+
+    const formatedDate = new Intl.DateTimeFormat('en-GB', options).format(currentDate);
+
+    console.log(`[${formatedDate}] ${req.ip} HTTP/${req.httpVersion} ${req.method} ${req.hostname}${req.path}`)
+    next()
+})
 port = process.env.PORT || 3000;
+
+const route = require("./routes/workout")
+
+app.use(route)
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
