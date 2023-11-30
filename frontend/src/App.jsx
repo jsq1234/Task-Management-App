798ffa71b376a1cc73add3021f8c82daab07a6aa
jsq1234 <mananprakash3078@gmail.com>
2023-12-01 04:24:55 +0530

    updated frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="nav-bar">Hello World!</div>
+    </>
+  )
+}
+
+export default App
