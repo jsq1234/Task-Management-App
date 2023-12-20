17282e21b8afff65e72a1bf47fb87c3c6603fd68
jsq1234 <mananprakash3078@gmail.com>
2023-12-20 18:38:40 +0530

    added logic for login user and sending jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,35 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '7d'})
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '7d' })
 }
 // login users
 const loginUser = async (req, res) => {
-    res.json({ message: 'Login User' });
+
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error('Incorrect email.');
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error('Incorrect password.');
+        }
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
+
+    } catch (error) {
+        console.log(`Error : ${error.message}`);
+        res.status(400).json({ Error: error.message });
+    }
+
 }
 
 
@@ -15,6 +39,15 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
     try {
         const { email, user_name, password } = req.body;
+
+        // Check if the user has already signed up by using his/her email
+        const exists = await User.findOne({ email });
+
+        if (exists) {
+            throw new Error('Email already in use.');
+        }
+
+        // create hash password using bcrypt
         const salt = await bcrypt.genSalt(10);
         const password_hash = await bcrypt.hash(password, salt);
 
@@ -24,6 +57,7 @@ const signupUser = async (req, res) => {
         // create a JWT token 
         const token = createToken(user._id);
 
+        // send the JWT token so that the frontend can use it.
         res.status(200).json({ user_name, token });
     } catch (e) {
         console.log(`Error : ${e.message}`);
